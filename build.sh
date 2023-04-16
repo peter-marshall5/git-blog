3753c04b53901d33380c5b2e634e01fb71dd9fbf
Peter Marshall <petmshall@gmail.com>
2023-04-16 13:24:41 -0400

    Move resources to subdirectory and revamp homepage theme

--- build.sh
+++ build.sh
@@ -8,8 +8,12 @@ https_url="https://$domain"
 html_path="dist"
 html_index="$html_path/index.html"
 sitemap="$html_path/sitemap.xml"
+res_dir="res"
+html_res="$res_dir/html"
+gmi_res="$res_dir/gmi"
 blog_dir="blog"
 blog_path="$html_path/$blog_dir"
+title_char_limit=200
 
 if [ -e $html_path ]; then
 	rm $html_path -r
@@ -19,17 +23,18 @@ mkdir $html_path
 mkdir $blog_path
 
 function start_index () {
-	cat header.html | sed "s/\$NAME/$name/g" | sed "s/\$META/$meta/g" >> $html_index
-	cat header.gmi | sed "s/\$NAME/$name/g" > gmi/index.gmi
+	cat $html_res/header.html | sed "s/\$NAME/$name/g" | sed "s/\$META/$meta/g" >> $html_index
+	cat $gmi_res/header.gmi | sed "s/\$NAME/$name/g" > gmi/index.gmi
 	cat <<EOF > $sitemap
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
 EOF
 }
 
+
 function add_index () {
-	echo "<a href=\"$blog_dir/$1.html\">$2</a><br/>" >> $html_index
-	echo "=> $blog_dir/$1.gmi $3 $2" >> gmi/index.gmi
+	echo "<div><a href=\"$blog_dir/$1.html\"><h2>$2</h2><p>$3</p></a></div>" >> $html_index
+	echo "=> $blog_dir/$1.gmi $2 - $3" >> gmi/index.gmi
 	cat <<EOF >> $sitemap
   <url>
     <loc>$https_url/$blog_dir/$1</loc>
@@ -40,8 +45,8 @@ EOF
 }
 
 function end_index () {
-	cat footer.html | sed "s/\$COPYRIGHT/$copyright/g" >> $html_index
-	cat footer.gmi | sed "s/\$COPYRIGHT/$copyright/g" >> gmi/index.gmi
+	cat $html_res/footer.html | sed "s/\$COPYRIGHT/$copyright/g" >> $html_index
+	cat $gmi_res/footer.gmi | sed "s/\$COPYRIGHT/$copyright/g" >> gmi/index.gmi
 	echo "</urlset>" >> $sitemap
 }
 
@@ -95,11 +100,10 @@ function line_break () {
 }
 
 function generate () {
-	cat header-post.html | sed "s/\$TITLE/$2/" | sed "s/\$NAME/$name/g"
+	cat $html_res/header-post.html | sed "s/\$TITLE/$2/" | sed "s/\$NAME/$name/g"
 
 	local is_list=n
 	local is_pre=n
-	local first_line=n
 	while IFS= read -r line; do
 		if [[ "$is_pre" == "y" ]]; then
 			if [[ "$line" == '```'* ]]; then
@@ -123,10 +127,6 @@ function generate () {
 		fi
 		case "$line" in
 			'')
-				#if [ "$first_line" == "n" ]; then
-				#	line_break
-				#	first_line=y
-				#fi
 				continue
 				;;
 			'* '*)
@@ -142,10 +142,10 @@ function generate () {
 				heading 1 "${line/\# /}"
 				;;
 			'## '*)
-				heading 2 "${line/\## /}"
+				heading 2 "${line/\#\# /}"
 				;;
 			'### '*)
-				heading 3 "${line/\### /}"
+				heading 3 "${line/\#\#\# /}"
 				;;
 			'> '*)
 				quote "${line/\> /}"
@@ -160,31 +160,31 @@ function generate () {
 		first_line=n
 	done < $1
 
-	cat footer-post.html | sed "s/\$COPYRIGHT/$copyright/g"
+	cat $html_res/footer-post.html | sed "s/\$COPYRIGHT/$copyright/g"
 }
 
-cp style.css $html_path/
+cp $html_res/main.css $html_res/home.css $html_res/post.css $html_path/
 cp gmi/res/ $html_path/ -r
-cat robots.txt | sed "s/\$DOMAIN/$domain/" > $html_path/robots.txt
+cat $res_dir/robots.txt | sed "s/\$DOMAIN/$domain/" > $html_path/robots.txt
 start_index
-find gmi/blog/*.gmi | while read file; do
+find gmi/blog/*.gmi | sort | while read file; do
 	filename=${file/gmi\/blog\//}
 	basename="${filename/.gmi/}"
 	htmlpath="$blog_path/${filename/.gmi/.html}"
 	echo Build $filename
 
-	date=$(date -R -r "$file")
-	
 	title=$(cat $file | head -n 1)
 	if [[ "$title" =~ ^#.* ]]; then
 		title="${title/\# /}"
+		date=$(cat $file | head -n 2 | tail -n 1)
 	else
 		title="$basename"
+		date=$(date +"%b %d, %Y")
 	fi
-
-	add_index "$basename" "$title" "$date"
+	title="${title:0:$title_char_limit}"
 
 	generate "$file" "$title" > "$htmlpath"
-	touch -d "$date" "$htmlpath"
+
+	add_index "$basename" "$title" "$date"
 done
 end_index
